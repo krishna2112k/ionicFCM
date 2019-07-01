d7bcec7a129f4de6c8a808bb1bf2434c39b594f2
Vamsi Krishna K <vamsi.k@pennant.local>
2019-07-01 15:00:09 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { FCM } from '@ionic-native/fcm/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private fcm : FCM
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.initializeFCM();
+    });
+  }
+
+  initializeFCM(){
+    this.fcm.getToken().then(token => {
+      console.log("============token =================",token)
+    });
+    
+    this.fcm.onNotification().subscribe(data => {
+      if(data.wasTapped){
+        console.log("Received in background");
+      } else {
+        console.log("Received in foreground");
+      };
+    });
+    
+    this.fcm.onTokenRefresh().subscribe(token => {
+      console.log("============token refresh=================",token)
+    });
+    
+  }
+}
